627af6976e766d8b4c26147855d875721a3d23e6
Benjamin Wu <benjaminwu124@gmail.com>
2018-11-23 18:18:13 -0500

    Add local run method

--- src/main/scala/hello/Handler.scala
+++ src/main/scala/hello/Handler.scala
@@ -1,12 +1,19 @@
 package hello
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
-import scala.collection.JavaConverters
 
 class Handler extends RequestHandler[Request, Response] {
 
   def handleRequest(input: Request, context: Context): Response = {
     println("Hello world")
+    println(context)
     Response("Hello world", input)
+  }
+}
+
+object LocalTest {
+  def main(args: Array[String]): Unit = {
+    val input = new Request("a", "b", "c")
+    println(new Handler().handleRequest(input, null))
   }
 }
